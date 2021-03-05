8a0618336aac0f51adfbcc797d02f07f17ff6607
Jan Kühle <jan.kuehle@codecentric.de>
2021-03-05 15:45:56 +0100

    Update prometheus example.sh

--- examples/prometheus/example.sh
+++ examples/prometheus/example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -xeuo pipefail
 
 export OTEL_TRACES_EXPORTER=none
 export OTEL_METRICS_EXPORTER=prometheus
@@ -12,47 +12,40 @@ tracebuild="$HERE/../../target/debug/tracebuild"
 BUILD_ID=$($tracebuild id)
 BUILD_START=$($tracebuild now)
 
-$tracebuild cmd \
-	--build $BUILD_ID \
-	-- \
-	sleep $((1 + $RANDOM % 5))
-
 STEP_ID=$($tracebuild id)
 STEP_START=$($tracebuild now)
-$tracebuild cmd \
-	--build $BUILD_ID \
-	--step $STEP_ID \
-	-- \
-	sleep $((1 + $RANDOM % 50))
-$tracebuild cmd \
-	--build $BUILD_ID \
-	--step $STEP_ID \
-	-- \
-	sleep $((1 + $RANDOM % 100))
-$tracebuild step \
-	--build $BUILD_ID \
-	--id $STEP_ID \
-	--start-time $STEP_START \
-	--name build \
-	--status success
+sleep 1 # download tracebuild
+$tracebuild step --build $BUILD_ID --id $STEP_ID --start-time $STEP_START --name Setup --status success
 
-STEP_ID=$($tracebuild id)
-STEP_START=$($tracebuild now)
-$tracebuild cmd \
-	--build $BUILD_ID \
-	--step $STEP_ID \
-	-- \
-	sleep $((1 + $RANDOM % 100))
-$tracebuild step \
-	--build $BUILD_ID \
-	--id $STEP_ID \
-	--start-time $STEP_START \
-	--name test \
-	--status success
+test() {
+	TEST_STEP_ID=$($tracebuild id)
+	TEST_STEP_START=$($tracebuild now)
 
-$tracebuild build \
-	--id $BUILD_ID \
-	--start-time $BUILD_START \
-	--name example \
-	--status success \
-	--branch main
+	STEP_START=$($tracebuild now)
+	sleep $((12 + $RANDOM % 6))
+	$tracebuild step --build $BUILD_ID --step $TEST_STEP_ID --id $($tracebuild id) --start-time $STEP_START --name "Install toolchain" --status success
+
+	$tracebuild cmd --build $BUILD_ID --step $STEP_ID --name "cargo test" -- sleep $((120 + $RANDOM % 60))
+
+	$tracebuild step --build $BUILD_ID --id $TEST_STEP_ID --start-time $TEST_STEP_START --name test --status success
+}
+
+lint() {
+	LINT_STEP_ID=$($tracebuild id)
+	LINT_STEP_START=$($tracebuild now)
+
+	STEP_START=$($tracebuild now)
+	sleep $((12 + $RANDOM % 6))
+	$tracebuild step --build $BUILD_ID --step $LINT_STEP_ID --id $($tracebuild id) --start-time $STEP_START --name "Install toolchain" --status success
+
+	$tracebuild cmd --build $BUILD_ID --step $STEP_ID --name "cargo fmt" -- sleep 1
+	$tracebuild cmd --build $BUILD_ID --step $STEP_ID --name "cargo clippy" -- sleep $((75 + $RANDOM % 30))
+
+	$tracebuild step --build $BUILD_ID --id $LINT_STEP_ID --start-time $LINT_STEP_START --name lint --status success
+}
+
+test &
+lint &
+wait
+
+$tracebuild build --id $BUILD_ID --start-time $BUILD_START --name "tracebuild - Example Prometheus" --status success --branch main
